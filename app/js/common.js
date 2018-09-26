65a128799cc932e3569c03b22e9fe1d1b7fda7e1
Sergey Klitko <sergey.klitko@netzlabor-coding.de>
2018-09-26 16:57:06 +0300

    add mmenu

--- app/js/common.js
+++ app/js/common.js
@@ -182,3 +182,28 @@ $(function() {
     preloadImages($(this).data('img'))
   })
 })
+
+$(function() {
+  var $menu = $('#my-menu').mmenu({
+    extensions: ['position-right'],
+    navbar: { add: false },
+    slidingSubmenus: false
+  })
+  var $icon = $('#my-icon')
+  var API = $menu.data('mmenu')
+
+  $icon.on('click', function() {
+    API.open()
+  })
+
+  API.bind('open:finish', function() {
+    setTimeout(function() {
+      $icon.addClass('is-active')
+    }, 100)
+  })
+  API.bind('close:finish', function() {
+    setTimeout(function() {
+      $icon.removeClass('is-active')
+    }, 100)
+  })
+})
